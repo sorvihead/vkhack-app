13b03ef88bd709457c888dcd0a7d1fc4f2af4966
sorvihead <37163446+sorvihead@users.noreply.github.com>
2019-09-28 00:06:56 +0300

    deleted tokens in master branches

--- app/models.py
+++ app/models.py
@@ -22,28 +22,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    token = db.Column(db.String(32), index=True, unique=True)
-    token_expiration = db.Column(db.DateTime)
-
-    def get_token(self, expires_in=3600):
-        now = datetime.utcnow()
-        if self.token and self.token_expiration > now + timedelta(seconds=60):
-            return self.token
-        self.token = base64.b64encode(os.urandom(24).decode('utf-8'))
-        self.token_expiration = now + timedelta(seconds=expires_in)
-        db.session.add(self)
-        return self.token
     
-    def revoke_token(self):
-        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
-
-    @staticmethod
-    def check_token(token):
-        user = User.query.filter_by(token=token).first()
-        if user is None or user.token_expiration < datetime.utcnow():
-            return None
-        return user
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
