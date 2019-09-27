580610829f531dd0a200921125ec9a8353a3671c
sorvihead <37163446+sorvihead@users.noreply.github.com>
2019-09-27 22:40:50 +0300

    auth with tokens

---
+++ config.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['sorvihead1@gmail.com', 'novoid86@yandex.ru']
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or None
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
